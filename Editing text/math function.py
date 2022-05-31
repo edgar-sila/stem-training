552fbf3e644bf82d1c18afb7c6dc4d93e06bed27
= <edigarsila.1@gmail.com>
2022-05-31 06:23:16 +0300

    updated

---
+++ Editing text/math function.py
@@ -0,0 +1,13 @@
+import math
+print(math.ceil(9.5))
+print  (  math.tan(3))
+print ( math.log(2))
+print  (math.sin(1))
+print   (math.pow(8,5))
+
+import calendar
+yy=2004
+mm=2
+print(calendar.month(yy,mm))
+
+from class import Person
